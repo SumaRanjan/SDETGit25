75e5e1f07c0c4acc323408ade00c60f339e3ab71
kranjan <kranjan@RanjanKumar>
2021-12-09 15:25:01 +0530

    Added sowmya srivalli

--- src/test/java/Practice/DemoTest.java
+++ src/test/java/Practice/DemoTest.java
@@ -7,5 +7,7 @@ public class DemoTest {
 public void Demo() {
 	System.out.println("user-1 script-1");
 	System.out.println("Hiiii...... am here");
+	System.out.println("Sowmya ......!!!!");
+	System.out.println("Srivalliiiiiiii");
 }
 }
